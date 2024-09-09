3c99925f6d3f6ac830dc8cb5ef788ba1b8a3fb37
btissam19 <btissam1234btissam12@gmail.com>
2024-09-09 04:29:44 +0100

    adding real time info

--- app/controllers/generatequetions.py
+++ app/controllers/generatequetions.py
@@ -3,23 +3,17 @@ from fastapi import HTTPException
 from app.prompts.prompts import question_prompt_template ,advice_template
 from app.config.llm import llm
 from typing import List
-from typing import Dict
 from langchain.agents import load_tools, initialize_agent, AgentType
 
 async def generate_interview_questions():
-    # Retrieve extracted information from MongoDB
     document = await extracted_info_collection.find_one()
     if not document or not document.get("content"):
         raise HTTPException(status_code=404, detail="No extracted information found.")
-    
     extracted_info = document.get("content")
-    
-    # Format the prompt with extracted information
     question_prompt = question_prompt_template.format(**extracted_info)
     
     try:
-        # Generate interview questions using LLM
-        question_output =  llm.invoke(question_prompt)  # Ensure this is awaited if it's an async call
+        question_output =  llm.invoke(question_prompt) 
         print("Raw question output:", question_output)
         return {"questions": question_output}
     except Exception as e:
@@ -27,7 +21,7 @@ async def generate_interview_questions():
 
 
 def is_satisfactory_answer(answer: str) -> bool:
-    return "I do not have access" not in answer and len(answer) > 50  # Example condition
+    return "I do not have access" not in answer and len(answer) > 50 
 
 def is_relevant_response(question: str, response: str) -> bool:
     verification_prompt = f"Check if the following response answers the question effectively. If it does, respond with 'Yes', otherwise respond with 'No'.\nQuestion: {question}\nResponse: {response}"
@@ -51,25 +45,17 @@ async def get_answers_to_questions(questions: List[str]) -> List[str]:
     return answers
 
 async def generate_advices():
-    # Retrieve extracted information from MongoDB
     document = await extracted_info_collection.find_one()
     if not document or not document.get("content"):
         raise HTTPException(status_code=404, detail="No extracted information found.")
     
     extracted_info = document.get("content")
-    
-    # Format resume details for prompt
     resume_details = "\n".join([f"{key}: {value}" for key, value in extracted_info.items()])
-    
-    # Create the prompt for the LLM
-    
-    
     advice_prompt = advice_template.format(
         resume_details=resume_details
     )
     
     try:
-        # Invoke LLM to perform the comparison
         comparison_output = llm.invoke(advice_prompt )
         return {"advice_results": comparison_output}
     except Exception as e:

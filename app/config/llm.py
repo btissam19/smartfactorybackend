ecd911deac70916102941cbab75bc1e481377ada
ibtissam ech-chaibi <123760378+btissam19@users.noreply.github.com>
2025-01-01 18:05:52 +0100

    Update llm.py

--- app/config/llm.py
+++ app/config/llm.py
@@ -1,5 +1,5 @@
 from langchain_google_genai import GoogleGenerativeAI
-llm = GoogleGenerativeAI(model="gemini-1.5-flash",google_api_key="AIzaSyAz4mqqTvqCJ49E7Xv7tuvku-w68kVhjxk",generation_config={"response_mime_type": "application/json"})
+llm = GoogleGenerativeAI(model="gemini-1.5-flash",google_api_key="AIzaSyAdqWn3x7DBe9stQbR4OASbqVJ6UoafDNg",generation_config={"response_mime_type": "application/json"})
 
 
 
